1372821260c6d361046e690ee9d5a8d75544cda8
Peter <petersp2000@gmail.com>
2022-06-02 12:50:00 -0700

    Betty style implementation on 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0
+ * prints with proper grammar, but the outcome is a piece of art,
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return(0);
+	return (0);
 }
