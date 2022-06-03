a600356a41d1ddce19f5af8da478d81bebfaae82
Peter <petersp2000@gmail.com>
2022-06-03 16:07:28 -0700

    Adjusted 5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -8,11 +8,10 @@ int main(void)
 {
 	int dec;
 
-	for (dec = 0; dec < 10 ; dec++)
+	for (dec = 0; dec < 10; dec++)
 	{
-		printf("%i",dec);
+		printf("%i", dec);
 	}
 	printf("\n");
 	return (0);
 }
-
