5b74d1a711fd02d80d91e71942e9cf95f6ce6bf9
shady-cj <petersp2000@gmail.com>
2022-06-07 10:34:05 +0100

    Write a program that prints _putchar, followed by a new line.

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints the number of arguments passed to the program(Entry point)
+ * Return: Always 0 (Success)
+ * 
+ */
+
+int main(void)
+{
+    printf("_putchar\n");
+    return (0); 
+}
