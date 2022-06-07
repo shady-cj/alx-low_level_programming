66040c8457b1891d79f2614e0b925a0128ef612a
shady-cj <petersp2000@gmail.com>
2022-06-07 20:44:11 +0100

    Write a function that checks for lowercase character.

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,11 @@
+#include <ctype.h>
+#include "main.h"
+
+int _islower(int c)
+{
+    if (islower(c))
+    {
+        return (1);
+    }
+    return (0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -27,4 +27,14 @@ void print_alphabet_x10(void);
  * Return: void
  * Description: prints the alphabet 10 times
  */
+
+int _islower(int c);
+/**
+ * _islower - checks for lowercase character
+ * @c: The character to check
+ * Return: 1 if c is lowercase, 0 otherwise
+ * Description: checks for lowercase character
+ * Example: _islower('a') = 1
+ *         _islower('A') = 0
+ */ 
 #endif
