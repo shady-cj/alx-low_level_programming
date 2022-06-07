562b0ee7dab6a044058615dd059460dfdf2b7dad
shady-cj <petersp2000@gmail.com>
2022-06-07 22:13:47 +0100

    adding the absolute library to the print last digit function

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,9 +1,11 @@
 #include "main.h"
+#include <stdlib.h>
+
 int print_last_digit(int n)
 {
     int last_digit;
 
-    last_digit = n % 10;
+    last_digit = abs(n % 10);
     _putchar(last_digit + '0');
     return (last_digit);
 }
