0b834b8ba63e1aa2d83f0070de3f0f66f191af6b
Peter <petersp2000@gmail.com>
2022-06-07 13:14:25 -0700

    Added betty style

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,15 +1,19 @@
 #include "main.h"
+/**
+ * print_alphabet_x10 - prints all the aphabets
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	char c;
+	int i;
 
-void print_alphabet_x10(void){
-    char c;
-    int i;
-
-    for (i = 0; i < 10; i++)
-    {
-        for (c = 'a'; c <= 'z'; c++)
-        {
-            _putchar(c);
-        }
-        _putchar('\n');
-    }
+	for (i = 0; i < 10; i++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+	}
 }
