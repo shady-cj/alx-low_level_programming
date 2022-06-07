cd8b23979f87130a27f8ebc8478b1c3962dfe203
Peter <petersp2000@gmail.com>
2022-06-07 16:13:27 -0700

    Betty styling

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,8 +1,11 @@
 #include "main.h"
-
-
-
+/**
+ * add - adds two numbers
+ * @a: int The first number
+ * @b: int The second number
+ * Return: The sum of a and b
+ */
 int add(int a, int b)
 {
-    return (a + b);
+	return (a + b);
 }
--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,31 +1,37 @@
 #include "main.h"
-
+/**
+ * times_table - prints the time table
+ * @void: void
+ * Return: void
+ */
 void times_table(void)
 {
-    int i, j,mult;
+	int i, j, mult;
 
-    for (i = 0; i < 10; i++)
-    {
-        for (j = 0; j < 10; j++)
-        {
-            mult = i * j;
-            if (mult > 9){
-                _putchar((mult / 10) + '0');
-                _putchar((mult % 10) + '0');
-            }
-            else{
-                _putchar(mult + '0');
-            }
-            if (j < 9){
-                _putchar(',');
-                _putchar(' ');
-                if (i * (j + 1) < 10)
-                {
-                    _putchar(' ');
-                }
-            }
-           
-        }
-        _putchar('\n');
-    }
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			mult = i * j;
+			if (mult > 9)
+			{
+				_putchar((mult / 10) + '0');
+				_putchar((mult % 10) + '0');
+			}
+			else
+			{
+				_putchar(mult + '0');
+			}
+			if (j < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+				if (i * (j + 1) < 10)
+				{
+					_putchar(' ');
+				}
+			}
+		}
+		_putchar('\n');
+	}
 }
