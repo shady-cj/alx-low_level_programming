876b6cdda95b9f65f94fb9656560c9af9eed13d8
Peter <petersp2000@gmail.com>
2022-06-09 08:15:15 -0700

    Write a function that checks for uppercase character.

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+int _isupper(int c);
+/**
+ * _isupper - A function the returns 1 or 0 if a character is uppercase or not
+ * c: The character to check
+ * Return: An integer 1 or 0
+ */
+#endif
