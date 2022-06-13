0d2cd8a6dff842e36d3a8d96a904a51b099e6945
Peter <petersp2000@gmail.com>
2022-06-13 07:39:31 -0700

    updatedd main.h

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -8,4 +8,11 @@ void reset_to_98(int *n);
  * Return: void
  */
 
+void swap_int(int *a, int *b);
+/**
+ * swap_int - Swaps the value of 2 ints
+ * @a: First int
+ * @b: second int
+ * Return: void
+
 #endif
