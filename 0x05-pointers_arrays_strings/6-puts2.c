eea006617cefedd119d1ba14e3139b5661c736d6
shady-cj <petersp2000@gmail.com>
2022-06-13 18:43:16 +0100

    bug fixing

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -2,7 +2,7 @@
 #include <string.h>
 /**
  * puts2 - Prints every other character of a string starting with the first character
- * @str String to be printed
+ * @str: String to be printed
  * Return: void
  */
 
