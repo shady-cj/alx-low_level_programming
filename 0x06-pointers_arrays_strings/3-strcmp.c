69d0205afc9ea82b23de73de0478a91edb369d5e
Peter <petersp2000@gmail.com>
2022-06-15 12:40:10 -0700

    debug

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,3 +1,4 @@
+#include <string.h>
 /**
  * _strcmp - A function that compares two strings
  * @s1: The first string to be compared
@@ -6,7 +7,7 @@
  * @s1 > @s2, -1 if @s1 < @s2
  */
 
-int _strcmp(char *s1, char *s2);
+int _strcmp(char *s1, char *s2)
 {
 	int i, j, k, l, ret;
 
