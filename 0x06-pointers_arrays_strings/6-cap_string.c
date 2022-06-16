5967645ef075f046398aaa69366e6f8118ab53e1
Peter <petersp2000@gmail.com>
2022-06-16 08:15:21 -0700

    testing output

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,50 +8,35 @@
 
 char *cap_string(char *str)
 {
-	char s[14];
+	char s[14] = {' ', '\n', '\t', ',', ';', '.', '!', '?', '"', '\'', '(', ')', '{', '}'};
 	int len = (int) strlen(str);
 	int j, k, i;
 
 	i = 0;
 
-	s[0] = ' ';
-	s[1] = '\n';
-	s[2] = '\t';
-	s[3] = ',';
-	s[4] = ';';
-	s[5] = '.';
-	s[6] = '!';
-	s[7] = '?';
-	s[8] = '"';
-	s[9] = '\'';
-	s[10] = '(';
-	s[11] = ')';
-	s[12] = '{';
-	s[13] = '}';
-
-
 	while (i < len)
 	{
-		j = i - 1;
-		if (j >= 0)
-		{
-		k = 0;
+	j = i - 1;
 
-		while (k < 14)
-		{
-		if (str[j] == s[k])
-		{
-		str[i] = toupper(str[i]);
-		break;		
-		}
-		k++;
-		}	
-		}
-		else
-		{
-		str[i] = toupper(str[i]);
-		}
-		i++;
+	if (j >= 0)
+	{
+	k = 0;
+
+	while (k < 14)
+	{
+	if (str[j] == s[k])
+	{
+	str[i] = toupper(str[i]);
+	break;
+	}
+	k++;
+	}
+	}
+	else
+	{
+	str[i] = toupper(str[i]);
+	}
+	i++;
 	}
 	return (str);
 }
