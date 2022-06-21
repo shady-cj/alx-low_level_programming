ef032738496bce61d7ba37d4000ed7f5ecd55607
Peter <petersp2000@gmail.com>
2022-06-20 19:27:56 -0700

    big fix

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -23,7 +23,7 @@ char *_strstr(char *haystack, char *needle)
 
 		do {
 			if (needle[index + 1] == '\0')
-				return (needle);
+				return (haystack);
 			index++;
 		} while (haystack[index] == needle[index]);
 		}
