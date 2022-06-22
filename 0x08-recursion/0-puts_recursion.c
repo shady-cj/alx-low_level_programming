9d69f2fd2888315db3977e02d576acb544e0da69
shady-cj <petersp2000@gmail.com>
2022-06-22 14:04:57 +0100

    puts clone

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _puts-recursion - A function that prints
+ * a string
+ * @s: String to be printed
+ * Return: void
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar(*s);
+		_puts_recursion(s+1);
+	}
+}
