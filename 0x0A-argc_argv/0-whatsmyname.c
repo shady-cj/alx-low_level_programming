031cb294b5cf261b38ae1b8516da7f92d70fae46
shady-cj <petersp2000@gmail.com>
2022-06-24 10:45:51 +0100

    Write a program that prints its name, followed by a new line.

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - The entry point and it prints the program name
+ * @argc: The argument count or the number of command line
+ * arguments including the program name
+ * @argv: An array of the command line arguments
+ * Return: Returns and int
+ */
+
+int main(int argc, int *argv[])
+{
+	printf("%s\n", argv[0]);
+}
