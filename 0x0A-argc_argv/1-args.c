8511fb833ed8238d8ef66928d60ad6951f717851
shady-cj <petersp2000@gmail.com>
2022-06-24 10:56:01 +0100

    Write a program that prints all arguments it receives

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -12,4 +12,5 @@
 int main(int argc, char *argv[])
 {
 	printf("%d\n", argc);
+	return (0);
 }
--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
 /**
- * main - The entry point and it prints the program name
+ * main - The entry point and it prints the all the arguments passed
+ * separated with a new line
  * @argc: The argument count or the number of command line
  * arguments including the program name
  * @argv: An array of the command line arguments
@@ -10,6 +11,14 @@
 
 int main(int argc, char *argv[])
 {
-	printf("%s\n", argv[0]);
+	int i;
+
+	i = 0;
+
+	while (i < argc)
+	{
+		printf("%s\n", argv[i]);
+		i++;
+	}
 	return (0);
 }
