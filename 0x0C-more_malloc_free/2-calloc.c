192b29d1b5035a7439a98e5bc8fc2ad6d7bb4e68
shady-cj <petersp2000@gmail.com>
2022-06-30 17:21:58 +0100

    calloc functionality

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -8,13 +8,19 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
+	unsigned int i;
 	void *ret;
+	char *filler;
 
 	if (nmemb <= 0 || size <= 0)
 		return (NULL);
 
 	ret = malloc(nmemb * size);
+
 	if (ret == NULL)
 		return (NULL);
+	for (i = 0; i < (nmemb * size); i++)
+		filler[i] = '\0';
+
 	return (ret);
 }
