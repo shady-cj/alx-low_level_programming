d5326457cdfd18f5887261db55ed57a9af4c8a4a
shady-cj <petersp2000@gmail.com>
2022-06-30 22:34:28 +0100

    implementing realloc clone

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,42 @@
+#include <stdlib.h>
+/**
+ * _realloc - A function that reallocates a block using
+ * malloc and free
+ * @ptr: A pointer to the previously allocated block
+ * @old_size: The old size of the previous space in bytes
+ * @new_size: The new size to be reallocated
+ * Return: A void pointer to the newly allocated block
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	void *ret;
+	char *filler, *ptr2;
+
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	ret = malloc(new_size);
+	if (ptr == NULL)
+	{
+		free(ptr);
+		return (ret);
+	}
+	if (new_size <= old_size)
+	{
+		return (ptr);
+	}
+	if (ret == NULL)
+		return (NULL);
+	ptr2 = ptr;
+	filler = ret;
+	while(*ptr2)
+	{
+		*filler = *ptr2;
+		filler++;
+		ptr2++;
+	}
+	free(ptr);
+	return (ret);
+}
