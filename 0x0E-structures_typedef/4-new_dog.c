68218feae13a8c2df5692b814dce7b9717524810
shady-cj <petersp2000@gmail.com>
2022-07-05 00:23:45 +0100

    function that creates a new dog.

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include "dog.h"
+#include <string.h>
 /**
  * new_dog - a function that creates a copy of struct dog
  * @name: The name of the dog to be copied
@@ -10,9 +11,11 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *my_dog;
-	char *n = name;
-	char *o = owner;
-
+	char *n;
+	char *o;
+	
+	n = name;
+	o = owner;
 	my_dog = malloc(sizeof(dog_t));
 	if (my_dog == NULL)
 		return (NULL);
---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * free_dog - A function that frees the struct passed in
+ * @d: The struct to be released
+ * Return: void
+ */
+void free_dog(dog_t *d)
+{
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
