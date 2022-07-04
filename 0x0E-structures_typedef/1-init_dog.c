a9533fae3c22d1ecd86120a96aa88c723a3260be
shady-cj <petersp2000@gmail.com>
2022-07-04 23:03:55 +0100

    a new type struct dog with the following elements

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,4 @@
-
+#include "dog.h"
 /**
  * init_dog - a function that initializes a variable off type dog
  * @d: The variable holding the user defined data type
@@ -11,5 +11,5 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	d->name = name;
 	d->age = age;
-	d->owwner = owner;
+	d->owner = owner;
 }
