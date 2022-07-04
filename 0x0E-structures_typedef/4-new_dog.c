3b62b0e730b3d98242ff1b78ca881249f1449bc3
shady-cj <petersp2000@gmail.com>
2022-07-04 23:46:46 +0100

    function that creates a new dog.

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * new_dog - a function that creates a copy of struct dog
+ * @name: The name of the dog to be copied
+ * @age: The age of the dog
+ * @owner: The copy of the owner of the dog
+ * Return: The dog_t data type defined in struct dog
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *my_dog;
+	char *n = name;
+	char *o = owner;
+
+	my_dog = malloc(sizeof(dog_t));
+	
+	if (my_dog == NULL)
+		return (NULL);
+
+	(*my_dog).name = n;
+	(*my_dog).age = age;
+	(*my_dog).owner = o;
+
+	return (my_dog);
+
+}
