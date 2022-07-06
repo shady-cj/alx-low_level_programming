f56200bc8694ad0c36550742bf11e30d632f8e21
shady-cj <petersp2000@gmail.com>
2022-07-06 19:16:28 +0100

    implementing afunction that prints a name.

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 /**
  * print_name - A function that prints a name that's being
  * passed
@@ -7,5 +8,7 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
+	if (name == NULL || f == NULL)
+		return;
 	f(name);
 }
