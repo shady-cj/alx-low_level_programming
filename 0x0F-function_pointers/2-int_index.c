c279d110e12ab200317ebc426f9c6bc5ec06023d
shady-cj <petersp2000@gmail.com>
2022-07-06 20:23:32 +0100

    implementing afunction that prints each member of an array using a helper function

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+/**
+ * int_index - a function that searches for an integer.
+ * @array: The array of integers to search
+ * @size: The number of integers in the array
+ * @cmp: The helper function
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0 || array == NULL || cmp == NULL)
+		return (-1);
+	i = 0;
+	while(i < size)
+	{
+		if (cmp(arr[i]))
+			return (i);
+		i++;
+	}
+
+	return (-1);
+}
