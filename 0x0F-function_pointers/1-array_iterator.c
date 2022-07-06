0af8ab715de4520552f923a217c16d2bd58bc646
shady-cj <petersp2000@gmail.com>
2022-07-06 20:09:29 +0100

    implementing afunction that prints each member of an array using a helper function

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -4,11 +4,13 @@
  * given as a parameter on each element of an array
  * @array: The array to be printed
  * @action: The helper function
+ * @size: The size of the array
  * Return: void
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i;
+
 	if (array == NULL || size == 0 || action == NULL)
 		return;
 	i = 0;
