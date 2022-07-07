fd6f818c0d58f234f885b310f27906826e52b7d6
shady-cj <petersp2000@gmail.com>
2022-07-07 22:59:32 +0100

    function that prints strings, followed by a new line.

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,52 @@
+#include <stdarg.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+/**
+ * print_all - Prints all the variable parameters passed in
+ * @format: The formatter
+ * Return: Void
+ */
+
+void print_all(const char * const format, ...)
+{
+	int len, i, ci;
+	int cc;
+	double cf;
+	char *cs, *glob;
+	va_list var;
+
+	len = (int) strlen(format);
+
+	i = 0;
+	va_start(var, format);
+	while (i < len)
+	{
+		glob = "";
+		if (i < len - 1)
+			glob = ", ";
+		switch (format[i])
+		{
+			case 'c':
+				cc = va_arg(var, int);
+				printf("%c%s", cc, glob);
+				break;
+			case 'i':
+				ci = va_arg(var, int);
+				printf("%d%s", ci, glob);
+				break;
+			case 'f':
+				cf = va_arg(var, double);
+				printf("%lf%s", cf, glob);
+				break;
+			case 's':
+				cs = va_arg(var, char *);
+				printf("%s%s", cs, glob);
+				break;
+		}
+		i++;
+	}
+	va_end(var);
+	printf("\n");
+
+}
