4e506e2f5d38dffa0ae9125240d9d328947ec556
shady-cj <petersp2000@gmail.com>
2022-07-07 21:28:21 +0100

    function that prints strings, followed by a new line.

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -14,6 +14,8 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	if (separator == NULL)
 		separator = "";
+	if (n == 0)
+		return;
 	va_start(numbers, n);
 	for (i = 0; i < n; i++)
 	{
