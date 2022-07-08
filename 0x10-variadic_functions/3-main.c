81c9bcc1fc98619f8cbb4974d66e66908960d097
shady-cj <petersp2000@gmail.com>
2022-07-08 01:44:08 +0100

    Debugging

--- 0x10-variadic_functions/3-main.c
+++ 0x10-variadic_functions/3-main.c
@@ -1,5 +1,5 @@
 #include "variadic_functions.h"
-
+#include <stdlib.h>
 /**
  * main - check the code
  *
@@ -7,6 +7,6 @@
  */
 int main(void)
 {
-    print_all("ceis", ' ', 3, "stSchool");
+    print_all(NULL, ' ', 3, "stSchool");
     return (0);
 }
