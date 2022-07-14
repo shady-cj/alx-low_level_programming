88ce5e7addde03acadda4d84771eb4d7b634dfb7
Peter <petersp2000@gmail.com>
2022-07-14 16:02:00 -0700

    betty

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -6,7 +6,7 @@ void before_main(void) __attribute__((constructor));
  */
 void before_main(void)
 {
-	printf("You're beat! and yet, "
-			"you must allow,\n"
-			"I bore my house upon my back!\n");
+	printf("You're beat! and yet, ");
+	printf("you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
