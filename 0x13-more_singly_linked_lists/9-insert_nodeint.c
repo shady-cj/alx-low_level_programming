c54a8e4ec9d3110dc7a1bf6d5f8a2cbf72b80543
shady-cj <petersp2000@gmail.com>
2022-07-18 23:11:30 +0100

    inserting into a linked list

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -26,6 +26,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		*head = new;
 		return (*head);
 	}
+	if (*head == NULL)
+		return (NULL);
 	while (i < idx)
 	{
 		temp = ptr;
