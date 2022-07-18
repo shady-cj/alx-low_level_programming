3512f42a692fb21ae4bf3fdde187b96d9c478559
shady-cj <petersp2000@gmail.com>
2022-07-18 20:29:28 +0100

    function that frees a listint_t list

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * free_listint - This functions frees the linked list from
+ * memory
+ * @head: The pointer to the head
+ * Return: void
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *ptr = NULL;
+
+	ptr = head;
+
+	while(ptr != NULL)
+	{
+		free(ptr);
+		ptr = ptr->link;
+	}
+}
