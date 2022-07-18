6cc8d2a2a4907b5a0c516fd22b56fb7faed2f934
shady-cj <petersp2000@gmail.com>
2022-07-18 18:53:36 +0100

    a function that prints all the elements of a listint_t list.

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,31 @@
+	.text
+	.file	"test.c"
+	.globl	main                            // -- Begin function main
+	.p2align	2
+	.type	main,@function
+main:                                   // @main
+	.cfi_startproc
+// %bb.0:
+	stp	x29, x30, [sp, #-16]!           // 16-byte Folded Spill
+	mov	x29, sp
+	.cfi_def_cfa w29, 16
+	.cfi_offset w30, -8
+	.cfi_offset w29, -16
+	adrp	x0, .L.str
+	add	x0, x0, :lo12:.L.str
+	bl	printf
+	mov	w0, wzr
+	ldp	x29, x30, [sp], #16             // 16-byte Folded Reload
+	ret
+.Lfunc_end0:
+	.size	main, .Lfunc_end0-main
+	.cfi_endproc
+                                        // -- End function
+	.type	.L.str,@object                  // @.str
+	.section	.rodata.str1.1,"aMS",@progbits,1
+.L.str:
+	.asciz	"Hello, Holberton"
+	.size	.L.str, 17
+
+	.ident	"clang version 14.0.5"
+	.section	".note.GNU-stack","",@progbits
