f838d3e2bab3ebd7ba21ae3e4b0f3e7f9cfe9721
shady-cj <petersp2000@gmail.com>
2022-07-19 14:40:50 +0100

    deleting a node in a linked list

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -13,21 +13,30 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 
 	if (*head == NULL)
 		return (-1);
-	ptr = *head;
 
-	while (i < index - 1)
+	ptr = *head;
+	if (index == 0)
 	{
-		if (ptr == NULL)
+		temp = ptr->next;
+		*head = temp;
+		free(ptr);
+		ptr = NULL;
+		return (1);
+
+	}
+	while (i < index)
+	{
+		temp = ptr;
+		if (temp == NULL)
 			return (-1);
 		ptr = ptr->next;
 		i++;
 	}
-	if (ptr == NULL || ptr->next == NULL)
+	if (ptr == NULL)
 		return (-1);
-	temp = ptr;
-	temp->next = ptr->next->next;
-	free(ptr->next);
-	ptr->next = NULL;
+	temp->next = ptr->next;
+	free(ptr);
+	ptr = NULL;
 	return (1);
 
 }
