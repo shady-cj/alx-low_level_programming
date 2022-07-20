9f5481f06ca010e0acd9ae8ec9eefc11fb3b00ab
shady-cj <petersp2000@gmail.com>
2022-07-20 02:19:22 +0100

    eliminating loops in linked list

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -1,1 +1,35 @@
-listint_t *find_listint_loop(listint_t *head);
+#include "lists.h"
+/**
+ * find_listint_loop - The functions finds and returns the start of
+ * loop in a linked list
+ * @head: The head node
+ * Return: The pointer to the start of the loop
+ */
+listint_t *find_listint_loop(listint_t *head)
+{
+	listint_t *hare = NULL;
+	listint_t *tortoise = NULL;
+	
+	if (head == NULL || head->next == NULL)
+		return (NULL);
+	hare = head->next->next;
+	tortoise = head;
+
+	while (hare != NULL)
+	{
+		if (hare == tortoise)
+		{
+			tortoise = head;
+			while (tortoise != hare)
+			{
+				hare = hare->next;
+				tortoise = tortoise->next;
+			}
+			return (tortoise);
+		}
+
+		hare = hare->next->next;
+		tortoise = tortoise->next;
+	}
+	return (NULL);
+}
