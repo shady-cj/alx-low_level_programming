8a9791d36e314978cc33190da82888cacec6f358
shady-cj <petersp2000@gmail.com>
2022-07-26 00:58:54 +0100

    creating and  writing a text to a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,7 +15,10 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	len = strlen(text_content);
 
-	fd = creat(filename, 0600);
+	/**
+	 * fd = creat(filename, 0600);
+	 */
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
 	w = write(fd, text_content, len);
