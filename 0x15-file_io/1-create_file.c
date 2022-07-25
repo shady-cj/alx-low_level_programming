94ccdcec11ecb0b7fe03c21d8fa153aff619c986
shady-cj <petersp2000@gmail.com>
2022-07-26 00:47:22 +0100

    creating and  writing a text to a file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * create_file - a function that creates a file
+ * @filename: The filename to create
+ * @text_content: The text to write into the file
+ * Return: 1 on success -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	size_t len = 0;
+	int fd;
+	ssize_t w;
+
+	if (filename == NULL)
+		return (-1);
+	len = strlen(text_content);
+
+	fd = creat(filename, 0600);
+	if (fd == -1)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		w = write(fd, text_content, len);
+		if (w == -1)
+			return (-1);
+	}
+
+	if (close(fd) == -1)
+		return (-1);
+	return (1);
+	
+
+}
