1b1ca26996f4e8b342518c7da78f700bcf11708e
shady-cj <petersp2000@gmail.com>
2022-09-22 23:03:48 +0100

    Fixing errors andncatching edge cases

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -20,7 +20,10 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table->size = size;
 	hash_table->array = malloc(sizeof(hash_node_t) * size);
 	if (hash_table->array == NULL)
+	{
+		free(hash_table);
 		return (NULL);
+	}
 	for (i = 0; i < size; i++)
 		hash_table->array[i] = NULL;
 	return (hash_table);
