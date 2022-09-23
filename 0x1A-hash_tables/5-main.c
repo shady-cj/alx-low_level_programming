4569d8c0cea52eee06b27fb829304a4cde739f1b
shady-cj <petersp2000@gmail.com>
2022-09-23 20:35:30 +0100

    Trying to merge from upstream

--- 0x1A-hash_tables/5-main.c
+++ 0x1A-hash_tables/5-main.c
@@ -16,11 +16,13 @@ int main(void)
     hash_table_print(ht);
     hash_table_set(ht, "c", "fun");
     hash_table_set(ht, "python", "awesome");
+    hash_table_print(ht);
     hash_table_set(ht, "Bob", "and Kris love asm");
     hash_table_set(ht, "N", "queens");
     hash_table_set(ht, "Asterix", "Obelix");
     hash_table_set(ht, "Betty", "Cool");
     hash_table_set(ht, "98", "Battery Street");
+    hash_table_set(ht, "hetairas", "Value");
     hash_table_set(ht, "mentioner", "cool");
     hash_table_set(ht, "heliotropes", "neurospora");
     hash_table_set(ht, "neurospora", "cool");
@@ -28,6 +30,10 @@ int main(void)
     hash_table_set(ht, "serafins", "cool");
     hash_table_set(ht, "serafins", "bad");
     hash_table_set(ht, "neurospora", "okay");
+    hash_table_set(ht, "mentioner", "name");
+    hash_table_set(ht, "depravement", "okay");
+    hash_table_set(ht, "hetairas", "cooler");
+    hash_table_set(ht, "heliotropes", "Cooler");
 
 
     hash_table_print(ht);
