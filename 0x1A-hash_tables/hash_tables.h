fab2c0b6d8489492d1a34f4f46eb2decdab6b1a5
Peter <petersp2000@gmail.com>
2022-09-23 15:37:39 -0700

    betty coding style

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -89,7 +89,7 @@ void hash_table_delete(hash_table_t *ht);
 /* SORTED HASH TABLE */
 shash_table_t *shash_table_create(unsigned long int size);
 int shash_table_set(shash_table_t *ht, const char *key,
-                const char *value);
+		const char *value);
 
 char *shash_table_get(const shash_table_t *ht, const char *key);
 void shash_table_print(const shash_table_t *ht);
