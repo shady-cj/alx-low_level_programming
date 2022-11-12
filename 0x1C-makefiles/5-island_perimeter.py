b51ed1647d6574a86b5939fb319e0e2aad6e2db3
shady-cj <petersp2000@gmail.com>
2022-11-12 01:11:07 +0100

    first island perimeter attempt

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -10,10 +10,31 @@ def island_perimeter(grid):
     """
     
     sides = 0
-    
+    nodes = []
+    visited = set([])
     for i in range(len(grid)):
+        if len(nodes) > 0:
+            break
         for  j in range(len(grid[i])):
             if grid[i][j] == 1:
-                if grid[i][j+1] == 1:
-                    
-                sides += 1
+                nodes.append((i,j))
+
+    while len(nodes) > 0:
+        currentNode = nodes.pop(0)
+        if visited.__contains__(currentNode):
+            continue
+        visited.add(currentNode)
+        sides += 1
+        i, j = currentNode
+        i += 1
+        if i >= len(grid) or grid[i][j] == 0:
+            continue
+        k = j
+        while k >= 0 and grid[i][k] == 1:
+            nodes.append((i, k))
+            k -= 1
+        k = j
+        while k < len(grid[i]) and grid[i][k] == 1:
+            nodes.append((i, k))
+            k += 1
+    return sides * 2
