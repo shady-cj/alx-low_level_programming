ca7931406cab1610dc3497ff10ede9f1a68131e2
shady-cj <petersp2000@gmail.com>
2022-11-12 08:40:49 +0100

    Island perimeter

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -4,37 +4,31 @@
 This module returns the perimeter of the island described in grid
 """
 
+
 def island_perimeter(grid):
     """
     Calculating the perimeter of an island
     """
-    
-    sides = 0
-    nodes = []
-    visited = set([])
+    perimeter = 0
+    y_len = len(grid) - 1
+    x_len = len(grid[0]) - 1
     for i in range(len(grid)):
-        if len(nodes) > 0:
-            break
-        for  j in range(len(grid[i])):
+        for j in range(len(grid[i])):
+            top = i - 1
+            bottom = i + 1
+            left = j - 1
+            right = j + 1
             if grid[i][j] == 1:
-                nodes.append((i,j))
-
-    while len(nodes) > 0:
-        currentNode = nodes.pop(0)
-        if visited.__contains__(currentNode):
-            continue
-        visited.add(currentNode)
-        sides += 1
-        i, j = currentNode
-        i += 1
-        if i >= len(grid) or grid[i][j] == 0:
-            continue
-        k = j
-        while k >= 0 and grid[i][k] == 1:
-            nodes.append((i, k))
-            k -= 1
-        k = j
-        while k < len(grid[i]) and grid[i][k] == 1:
-            nodes.append((i, k))
-            k += 1
-    return sides * 2
+                if ((top >= 0 and grid[top][j] == 0) or
+                   top < 0):
+                    perimeter += 1
+                if ((bottom <= y_len and grid[top][j] == 0) or
+                   top > y_len):
+                    perimeter += 1
+                if ((left >= 0 and grid[i][left] == 0) or
+                   left < 0):
+                    perimeter += 1
+                if ((right <= x_len and grid[i][right] == 0) or
+                   right > x_len):
+                    perimeter += 1
+    return perimeter
